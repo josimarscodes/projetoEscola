36a59c91582d1433fab30dd3366e30c98b453a97
josimarsousa <josimarjosesousa@gmail.com>
2021-10-16 09:19:57 -0300

    Initial commit

---
+++ src/classes/Aluno.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Aluno {
+
+    Long codAluno;
+    String nomeAluno;
+    int idadeAluno;
+    String dataNascimentoAluno;
+    String numeroCpfAluno;
+    String rgAluno;
+    String nomeMaeAluno;
+    String nomePaiAluno;
+    String dataMatriculaAluno;
+    String serieMatriculaAluno;
+
+}
